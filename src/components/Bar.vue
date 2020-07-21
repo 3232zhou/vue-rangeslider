7836ee781a85c29c1d902f8e464780528c86b10c
forbid403 <forbid403@naver.com>
2020-07-21 09:54:43 +0900

    feat: Create default bar

---
+++ src/components/Bar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="range-slider__bar" :style="barStyles"></div>
+</template>
+
+
+<script>
+export default {
+  name: 'Bar',
+  props: {
+    barWidth: {
+      type: String,
+      default: '100%',
+    },
+    barHeight: {
+      type: String,
+      default: '8px',
+    },
+  },
+  computed: {
+    barStyles() {
+      return {
+        width: this.barWidth,
+        height: this.barHeight,
+        backgroundColor: 'cadetblue',
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/RangeSlider.vue
+++ src/components/RangeSlider.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{msg}}</h1>
+    <bar></bar>
   </div>
 </template>
 
 <script>
+import Bar from './Bar';
+
 export default {
   name: 'RangeSlider',
+  components: {
+    bar: Bar,
+  },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
