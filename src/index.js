48904b02b3504ed9452327862e06170bc6d21a7d
forbid403 <forbid403@naver.com>
2020-07-27 22:53:02 +0900

    refactor: Fix index.js for npm deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+import Vue from 'vue';
 import RangeSlider from './components/RangeSlider.vue';
-
-const install = (Vue) => {
-  Vue.component(RangeSlider);
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('RangeSlider', RangeSlider);
 }
 
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+// Create module definition for Vue.use()
+const plugin = {
+  install
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
 }
 
-export {
-  RangeSlider
-}
-
-export default install
+// To allow use as module (npm/webpack/etc.) export component
+export default RangeSlider;
