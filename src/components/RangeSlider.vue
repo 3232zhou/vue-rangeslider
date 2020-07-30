6b4d82eacfdc9e874755bc06eea659e3c4200206
JelloCho <jello.cho@key-inside.com>
2020-07-30 15:05:11 +0900

    remove scoped style

--- src/components/RangeSlider.vue
+++ src/components/RangeSlider.vue
@@ -1,64 +1,68 @@
 <template>
   <div class="root">
     <div class="range-slider">
-      <bar class="range-slider__bar" :barOptions="barOptions" ref="bar" :max="max" :sliceNum="sliceNum"></bar>
+      <bar
+        class="range-slider__bar"
+        :barOptions="barOptions"
+        ref="bar"
+        :max="max"
+        :sliceNum="sliceNum"
+      ></bar>
       <handle
-      class="range-slider__bar--min"
-      :handleOptions="handleOptions"
-      :tooltipOptions="tooltipOptions"
-      ref="handleMin"
-      :value="this.minValue"
-      type="min"></handle>
+        class="range-slider__bar--min"
+        :handleOptions="handleOptions"
+        :tooltipOptions="tooltipOptions"
+        ref="handleMin"
+        :value="this.minValue"
+        type="min"
+      ></handle>
 
       <handle
-      class="range-slider__bar--max"
-      :handleOptions="handleOptions"
-      :tooltipOptions="tooltipOptions"
-      ref="handleMax"
-      :value="this.maxValue"
-      type="max"></handle>
+        class="range-slider__bar--max"
+        :handleOptions="handleOptions"
+        :tooltipOptions="tooltipOptions"
+        ref="handleMax"
+        :value="this.maxValue"
+        type="max"
+      ></handle>
     </div>
-    <range
-    :min="min"
-    :max="max"
-    :sliceNum="sliceNum"
-    ></range>
+    <range :min="min" :max="max" :sliceNum="sliceNum"></range>
   </div>
 </template>
 
 <script>
-import Bar from './Bar';
-import Handle from './Handle';
-import Range from './Range';
+import Bar from "./Bar";
+import Handle from "./Handle";
+import Range from "./Range";
 
 export default {
-  name: 'RangeSlider',
+  name: "RangeSlider",
   components: {
     bar: Bar,
     handle: Handle,
-    range: Range,
+    range: Range
   },
   data() {
     return {
       barOptions: {
-        width: '100%',
-        height: '10px',
-        color: 'black',
-        sliceColor: '#049DBF',
+        width: "100%",
+        height: "10px",
+        color: "black",
+        sliceColor: "#049DBF"
       },
       handleOptions: {
         width: 12,
         height: 12,
-        color: '#D93D4A',
+        color: "#D93D4A"
       },
       tooltipOptions: {
         width: 20,
         height: 20,
-        color: '#F2C84B',
-        visibility: false,
+        color: "#F2C84B",
+        visibility: false
       },
       barWidth: {
-        type: Number,
+        type: Number
       },
       clickedHandle: null,
       initialMin: 0,
@@ -66,31 +70,31 @@ export default {
       minValue: 50,
       maxValue: 70,
       minPosition: 0,
-      maxPosition: 0,
+      maxPosition: 0
     };
   },
   props: {
     min: {
       type: Number,
-      default: 0,
+      default: 0
     },
     max: {
       type: Number,
-      default: 100,
+      default: 100
     },
     bar: {
-      type: Object,
+      type: Object
     },
     handle: {
-      type: Object,
+      type: Object
     },
     tooltip: {
-      type: Object,
+      type: Object
     },
     sliceNum: {
       type: Number,
-      default: 5,
-    },
+      default: 5
+    }
   },
   beforeMount() {
     this.setOptions();
@@ -98,7 +102,7 @@ export default {
   mounted() {
     this.barWidth = this.$refs.bar.$el.getBoundingClientRect().width;
     this.setInitialHandleValue();
-    document.addEventListener('mousedown', this.whichHandleClicked);
+    document.addEventListener("mousedown", this.whichHandleClicked);
     this.addKeyboardEvent();
   },
   methods: {
@@ -106,8 +110,10 @@ export default {
       this.initialMin = this.minValue;
       this.initialMax = this.maxValue;
 
-      this.minPosition = Math.abs(this.min - this.minValue) / (this.max - this.min);
-      this.maxPosition = Math.abs(this.min - this.maxValue) / (this.max - this.min);
+      this.minPosition =
+        Math.abs(this.min - this.minValue) / (this.max - this.min);
+      this.maxPosition =
+        Math.abs(this.min - this.maxValue) / (this.max - this.min);
 
       const minPercentage = this.minPosition * 100;
       const maxPercentage = this.maxPosition * 100;
@@ -121,70 +127,76 @@ export default {
     },
     whichHandleClicked(e) {
       e.preventDefault();
-      if (e.target === this.$refs.handleMin.$el) this.clickedHandle = this.$refs.handleMin;
-      else if (e.target === this.$refs.handleMax.$el) this.clickedHandle = this.$refs.handleMax;
+      if (e.target === this.$refs.handleMin.$el)
+        this.clickedHandle = this.$refs.handleMin;
+      else if (e.target === this.$refs.handleMax.$el)
+        this.clickedHandle = this.$refs.handleMax;
       else return;
 
       this.clickedHandle.$refs.handle.__vue__.handleHover();
       this.clickedHandle.$refs.handle.__vue__.clicked = true;
       this.clickedHandle.$refs.handle.visibility = true;
 
-      document.addEventListener('mousemove', this.onDrag);
-      document.addEventListener('mouseup', this.onDragEnd);
+      document.addEventListener("mousemove", this.onDrag);
+      document.addEventListener("mouseup", this.onDragEnd);
     },
     onDrag(e) {
       e.preventDefault();
       if (e.clientX <= 0) {
-        this.clickedHandle.$el.style.left = '0';
+        this.clickedHandle.$el.style.left = "0";
         this.minValue = this.initialMin;
         return;
       }
 
       if (e.clientX >= this.barWidth) {
-        this.clickedHandle.$el.style.left = 'initial';
-        this.clickedHandle.$el.style.right = '0';
+        this.clickedHandle.$el.style.left = "initial";
+        this.clickedHandle.$el.style.right = "0";
         this.maxValue = this.max;
         return;
       }
 
-      if (this.clickedHandle.$el.getAttribute('type') === 'max') {
+      if (this.clickedHandle.$el.getAttribute("type") === "max") {
         this.maxPosition = e.clientX / this.barWidth;
         const maxPercentage = this.maxPosition * 100;
-        this.maxValue = Math.round(this.maxPosition * (this.max - this.min)) + this.min;
+        this.maxValue =
+          Math.round(this.maxPosition * (this.max - this.min)) + this.min;
         this.clickedHandle.$el.style.left = `${maxPercentage}%`;
       }
 
-      if (this.clickedHandle.$el.getAttribute('type') === 'min') {
+      if (this.clickedHandle.$el.getAttribute("type") === "min") {
         this.minPosition = e.clientX / this.barWidth;
         const minPercentage = this.minPosition * 100;
-        this.minValue = Math.round(this.minPosition * (this.max - this.min)) + this.min;
+        this.minValue =
+          Math.round(this.minPosition * (this.max - this.min)) + this.min;
         this.clickedHandle.$el.style.left = `${minPercentage}%`;
       }
     },
     onDragEnd(e) {
       e.preventDefault();
 
-      document.removeEventListener('mousemove', this.onDrag);
-      document.removeEventListener('mouseup', this.onDragEnd);
+      document.removeEventListener("mousemove", this.onDrag);
+      document.removeEventListener("mouseup", this.onDragEnd);
       this.clickedHandle.$refs.handle.__vue__.clicked = false;
       this.clickedHandle.$refs.handle.__vue__.handleLeave();
     },
     addKeyboardEvent() {
-      window.addEventListener('keydown', (e) => {
+      window.addEventListener("keydown", e => {
         e.preventDefault();
         if (!this.clickedHandle) return;
 
         // left arrow
-        if (e.keyCode === 37) {}
+        if (e.keyCode === 37) {
+        }
         // right arrow
-        if (e.keyCode === 39) {}
+        if (e.keyCode === 39) {
+        }
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style>
 .range-slider {
   display: flex;
   position: relative;
@@ -205,10 +217,12 @@ export default {
 .range-slider__bar--max {
   position: absolute;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .1s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s;
 }
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 </style>
